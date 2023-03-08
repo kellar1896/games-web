22dbbe60a15a5ee0232b41d15740200086ac4051
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-08 22:12:17 +0200

    create first template of App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SideBar from './components/side-bar';
+import TopBar from './components/top-bar';
+import Router from './router';
 
 function App() {
   return (
-    <div className="min-h-screen bg-raisingBlack font-mono">
+    <div className="min-h-screen bg-raisingBlack font-mono flex flex-row">
       <SideBar />
+      <TopBar />
+      <div className='w-full md:w-9/12 min-h-screen'>
+        <Router />
+      </div>
     </div>
   );
 }
