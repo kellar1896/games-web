de6f52f3dddf76ba660fb925014c95c59f3c802c
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-09 14:26:12 +0200

    add icon to PageRoute type

--- src/models/routes.ts
+++ src/models/routes.ts
@@ -1,6 +1,10 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+
 export type PageRoute = {
     id: string;
+    name: String;
     path: string;
     element:()=> JSX.Element;
     children?: PageRoute[];
+    icon: IconProp
 }
