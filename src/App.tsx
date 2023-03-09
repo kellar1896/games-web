7ae89883fd66125e4d64c94c1756e54495727a48
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-09 14:23:43 +0200

    change component name

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import SideBar from './components/side-bar';
-import TopBar from './components/top-bar';
+import NavBar from './components/nav-bar';
 import Router from './router';
 
 function App() {
   return (
-    <div className="min-h-screen bg-raisingBlack font-mono flex flex-row">
+    <div className="min-h-screen bg-raisingBlack font-mono flex flex-col md:flex-row">
       <SideBar />
-      <TopBar />
+      <NavBar />
       <div className='w-full md:w-9/12 min-h-screen'>
         <Router />
       </div>
