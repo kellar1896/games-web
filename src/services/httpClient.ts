47e21e870fa7201bc6098102308e2d9db6c9d095
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-10 14:24:30 +0200

    create http client with necessary methods

---
+++ src/services/httpClient.ts
@@ -0,0 +1,51 @@
+
+export class HttpClient {
+  private baseUrl = process.env.API_URL;
+
+
+  async get(path: string) {
+    const response = await fetch(`${this.baseUrl}${path}`);
+    if (!response.ok) {
+      throw new Error(`HTTP ${response.status} - ${response.statusText}`);
+    }
+    return response.json();
+  }
+
+  async post(path: string, data: any) {
+    const response = await fetch(`${this.baseUrl}${path}`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP ${response.status} - ${response.statusText}`);
+    }
+    return response.json();
+  }
+
+  async put(path: string, data: any) {
+    const response = await fetch(`${this.baseUrl}${path}`, {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP ${response.status} - ${response.statusText}`);
+    }
+    return response.json();
+  }
+
+  async delete(path: string) {
+    const response = await fetch(`${this.baseUrl}${path}`, {
+      method: "DELETE",
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP ${response.status} - ${response.statusText}`);
+    }
+    return response.json();
+  }
+}
