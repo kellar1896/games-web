c384924302bd6b28fcf9d2c0c9e5425720369154
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-10 12:39:09 +0200

    moked data of games

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,9 +1,53 @@
 import react from 'react'
+import GamesTable from '../components/games-table'
+import { GameHeader } from '../models/games';
+
+const headers = ['id', 'name', 'description', 'creationDate'] as GameHeader[];
+const games = [
+  {
+    id: 1,
+    name: "Chess",
+    description: "A two-player strategy board game",
+    creationDate: "2018-01-01T00:00:00.000Z"
+  },
+  {
+    id: 2,
+    name: "Poker",
+    description: "A card game",
+    creationDate: "2018-01-01T00:00:00.000Z"
+  },
+  {
+    id: 3,
+    name: "Chess",
+    description: "A two-player strategy board game",
+    creationDate: "2018-01-01T00:00:00.000Z"
+  },
+  {
+    id: 4,
+    name: "Poker",
+    description: "A card game",
+    creationDate: "2018-01-01T00:00:00.000Z"
+  },
+  {
+    id: 5,
+    name: "Chess",
+    description: "A two-player strategy board game",
+    creationDate: "2018-01-01T00:00:00.000Z"
+  },
+  {
+    id: 6,
+    name: "Poker",
+    description: "A card game",
+    creationDate: "2018-01-01T00:00:00.000Z"
+  }
+  ]
 
 const Games = () => {
     return (
-        <div>
-            <h1>Games</h1>
+        <div className='p-2 md:p-10'>
+            <div className="">
+                <GamesTable headers={headers} data={games} />
+            </div>
         </div>
     )
 }
