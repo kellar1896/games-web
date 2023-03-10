f5a8d787dd4c2225d71fbf7ef1250fb9b8eaf180
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-10 12:38:21 +0200

    add more game types and create game headers type

--- src/models/games.ts
+++ src/models/games.ts
@@ -2,9 +2,12 @@ export type Game = {
     id: number;
     name: string;
     description: string;
+    creationDate: string;
 }
 
 export type GameConfiguration = {
     gameId: number;
     [key: string]: any;
 }
+
+export type GameHeader = 'id' | 'name' | 'description' | 'creationDate'
