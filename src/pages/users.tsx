e6c1a3563f2ecaff81203e7d8fd3233b51e29989
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-13 13:16:38 +0200

    use custom hook to fetch users data

--- src/pages/users.tsx
+++ src/pages/users.tsx
@@ -1,11 +1,30 @@
-import react from 'react'
+import react from "react";
+import UsersTable from "../components/molecules/users-table";
+import useFetchUsers from "../hooks/useFetchUsers";
+import { UserHeader } from "../models/users";
+
+const headers = ["name", "email", "location"] as UserHeader[];
 
 const Users = () => {
-    return (
-        <div>
-            <h1>Users</h1>
-        </div>
-    )
-}
+  const { usersData, isLoading, error } = useFetchUsers();
 
-export default Users
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        {`Error: ${error}`}
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-2 md:p-10">
+      <UsersTable headers={headers} data={usersData} onEditUser={() => {}} />
+    </div>
+  );
+};
+
+export default Users;
