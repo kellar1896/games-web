8590233bc80609819baff15bebe00503198d2b25
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-13 13:14:17 +0200

    user dashboard template as main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import './App.css';
-import SideBar from './components/side-bar';
-import NavBar from './components/nav-bar';
 import Router from './router';
+import MainDashboard from './components/templates/main-dashboard';
 
 function App() {
   return (
-    <div className="min-h-screen bg-raisingBlack font-mono flex flex-col md:flex-row">
-      <SideBar />
-      <NavBar />
-      <div className='w-full md:w-9/12 min-h-screen'>
+    <MainDashboard>
         <Router />
-      </div>
-    </div>
+    </MainDashboard>
   );
 }
 
