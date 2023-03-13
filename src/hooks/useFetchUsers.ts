2b3ec52eff88bd36f43b9890b12d032e5b1b5fe4
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-13 13:15:32 +0200

    create custom hook for fetch users data

---
+++ src/hooks/useFetchUsers.ts
@@ -0,0 +1,28 @@
+import { useEffect, useMemo, useState } from "react";
+import { User } from "../models/users";
+import { UserServices } from "../services/user.Services";
+
+export const useFetchUsers= () =>{
+    const [usersData, setUsersData] = useState([] as User[]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null as null | unknown);
+    const userServices = useMemo(()=> new UserServices(),[]);
+  
+    useEffect(() => {
+      const getUserData = async () => {
+        try {
+          const data = await userServices.fetchUsers();
+          setUsersData(data);
+          setIsLoading(false);
+        } catch (error) {
+          setError(error);
+          setIsLoading(false);
+        }
+      };
+      getUserData();
+    }, [userServices]);
+  
+    return { usersData, isLoading, error };
+}
+
+export default useFetchUsers;
