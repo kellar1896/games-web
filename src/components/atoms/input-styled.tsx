af3b6116a1490f6037610a1d4748e0a43bdec23f
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-13 13:12:29 +0200

    create styled input component with props

---
+++ src/components/atoms/input-styled.tsx
@@ -0,0 +1,41 @@
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import react from "react";
+
+type InputStyledProps = {
+  type?: react.HTMLInputTypeAttribute;
+  value: string;
+  onChange: (event: react.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+  icon?: IconProp;
+  id: string;
+};
+
+const InputStyled = ({
+  onChange,
+  type = "text",
+  value,
+  placeholder,
+  icon,
+  id,
+}: InputStyledProps) => {
+  return (
+    <div className="relative">
+      {icon && (
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <FontAwesomeIcon icon={icon} className="text-gray-400" />
+        </div>
+      )}
+      <input
+        id={id}
+        type={type}
+        className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+export default InputStyled;
