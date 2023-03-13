59f3cab111ebc8d6ce216131aba955831b5bbba4
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-13 13:16:52 +0200

    change fetch data process for custom hook

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,55 +1,73 @@
-import react from 'react'
-import GamesTable from '../components/games-table'
-import { GameHeader } from '../models/games';
+import react, { useCallback, useState } from "react";
+import EditGameCard from "../components/organisms/edit-game-card";
+import GamesTable from "../components/molecules/games-table";
+import { Game, GameHeader } from "../models/games";
+import useFetchGames from "../hooks/useFetchGames";
 
-const headers = ['id', 'name', 'description', 'creationDate'] as GameHeader[];
-const games = [
-  {
-    id: 1,
-    name: "Chess",
-    description: "A two-player strategy board game",
-    creationDate: "2018-01-01T00:00:00.000Z"
-  },
-  {
-    id: 2,
-    name: "Poker",
-    description: "A card game",
-    creationDate: "2018-01-01T00:00:00.000Z"
-  },
-  {
-    id: 3,
-    name: "Chess",
-    description: "A two-player strategy board game",
-    creationDate: "2018-01-01T00:00:00.000Z"
-  },
-  {
-    id: 4,
-    name: "Poker",
-    description: "A card game",
-    creationDate: "2018-01-01T00:00:00.000Z"
-  },
-  {
-    id: 5,
-    name: "Chess",
-    description: "A two-player strategy board game",
-    creationDate: "2018-01-01T00:00:00.000Z"
-  },
-  {
-    id: 6,
-    name: "Poker",
-    description: "A card game",
-    creationDate: "2018-01-01T00:00:00.000Z"
-  }
-  ]
+const headers = ["id", "name", "description", "creationDate"] as GameHeader[];
 
 const Games = () => {
-    return (
-        <div className='p-2 md:p-10'>
-            <div className="">
-                <GamesTable headers={headers} data={games} />
-            </div>
-        </div>
-    )
-}
+  const {games, isLoading, error } = useFetchGames();
+  const [selectedGame, setSelectedGame] = useState(null as null | Game);
+  const [formGame, setFormGame] = useState(null as null | Game);
 
-export default Games
+
+  const showSelectedGame = useCallback((game: Game) => {
+    console.log(`Selected game: ${JSON.stringify(game)}`);
+    setSelectedGame(game);
+    setFormGame(game);
+  }, []);
+
+  const handleForm = useCallback(
+    (e: react.ChangeEvent<HTMLInputElement>) => {
+      e.preventDefault();
+      setFormGame({
+        ...formGame,
+        [e.target.name]: e.target.value,
+      } as Game);
+    },
+    [formGame]
+  );
+
+  const onSubmit = useCallback(
+    (event: react.FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      console.log(`Form game: ${JSON.stringify(formGame)}`);
+      // put new form
+    },
+    [formGame]
+  );
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        {`Error: ${error}`}
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-2 md:p-10">
+      <GamesTable
+        headers={headers}
+        data={games}
+        onEditGame={showSelectedGame}
+      />
+      {selectedGame && (
+        <div className="my-5">
+          <EditGameCard
+            game={selectedGame}
+            onSubmit={onSubmit}
+            handleForm={handleForm}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Games;
