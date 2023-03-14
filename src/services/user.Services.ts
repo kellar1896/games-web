50a8316659851cdbf114821dacd2ef831f7b978c
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-14 12:54:15 +0200

    create services for edit created and delete users

--- src/services/user.Services.ts
+++ src/services/user.Services.ts
@@ -1,16 +1,50 @@
 import { User } from "../models/users";
+import { createId } from "../utils/tools";
 import { HttpClient } from "./httpClient";
 
 export class UserServices {
-    httpClient = new HttpClient();
-    
-    fetchUsers = async () => {
-        try {
-            const response = await this.httpClient.get("/users");
-            return response as User[];
-        } catch (error) {
-            console.log(`Error ${error}`);
-            throw new Error("Unable to fetch user data");
-        }
+  httpClient = new HttpClient();
+
+  fetchUsers = async () => {
+    try {
+      const response = await this.httpClient.get("/users");
+      return response as User[];
+    } catch (error) {
+      console.log(`Error ${error}`);
+      throw new Error("Unable to fetch user data");
     }
+  };
+
+  updateUser = async (userId: string, user: User) => {
+    try {
+      const response = await this.httpClient.put(`/users/${userId}`, user);
+      return response as User;
+    } catch (error) {
+      console.log(`Error ${error}`);
+      throw new Error("Unable to fetch user data");
+    }
+  };
+
+  createUser = async (user: User) => {
+    try {
+      const response = await this.httpClient.post("/users", {
+        ...user,
+        id: createId(),
+      });
+      return response as User;
+    } catch (error) {
+      console.log(`Error ${error}`);
+      throw new Error("Unable to fetch user data");
+    }
+  };
+
+  deleteUser = async (userId: string) => {
+    try {
+      const response = await this.httpClient.delete(`/users/${userId}`);
+      return response as User;
+    } catch (error) {
+      console.log(`Error ${error}`);
+      throw new Error("Unable to fetch user data");
+    }
+  }
 }
