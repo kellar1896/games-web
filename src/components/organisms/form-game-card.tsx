9a7c50a24ed89b9dfd69c37d2d5d90c8995ce3a6
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-14 15:59:14 +0200

    add error loading state and classname props

--- src/components/organisms/form-game-card.tsx
+++ src/components/organisms/form-game-card.tsx
@@ -15,6 +15,9 @@ type FormGameCardProp = {
       | React.ChangeEvent<HTMLSelectElement>
   ) => void;
   gameOptions: string[];
+  isLoading?: boolean;
+  errorForm?: string | null;
+  className?: string;
 };
 
 const FormGameCard = ({
@@ -22,15 +25,18 @@ const FormGameCard = ({
   onSubmit,
   handleForm,
   gameOptions,
+  isLoading = false,
+  errorForm,
+  className,
 }: FormGameCardProp) => {
   return (
     <form
       onSubmit={onSubmit}
-      className="w-full flex flex-col rounded-lg space-x-2"
+      className={`text-lavanderBlush w-full flex flex-col rounded-lg space-x-2 ${className}`}
     >
       <div className="flex flex-col md:flex-row md:justify-between">
         <div className="m-0 md:mr-10">
-          <div className="text-lavanderBlush text-sm my-2 w-full">
+          <div className="text-sm my-2 w-full">
             <label htmlFor="name">Name</label>
             <InputStyled
               value={game.name}
@@ -39,7 +45,7 @@ const FormGameCard = ({
               name="name"
             />
           </div>
-          <div className="text-lavanderBlush text-sm my-2 w-full">
+          <div className="text-sm my-2 w-full">
             <label htmlFor="description">Description</label>
             <TextAreaStyled
               value={game.description}
@@ -55,11 +61,15 @@ const FormGameCard = ({
               placeholder="select an option"
               onChange={handleForm}
               name="category"
+              value={game.category}
+              className={className}
             />
           </div>
         </div>
       </div>
-      <ButtonStyled type="submit" className="w-full md:w-20 self-end mt-5">
+      {errorForm && <p className="text-sm
+       font-bold text-red-600 my-2">{errorForm}</p>}
+      <ButtonStyled type="submit" className="w-full md:w-20 self-end mt-5" loading={isLoading}>
         Save
       </ButtonStyled>
     </form>
