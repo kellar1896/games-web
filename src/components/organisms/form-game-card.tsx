a04511ba40a4aa01afa671be253c7667a58274cd
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-14 14:45:05 +0200

    add game options properties

--- src/components/organisms/form-game-card.tsx
+++ src/components/organisms/form-game-card.tsx
@@ -3,6 +3,7 @@ import { Game } from "../../models/games";
 import ButtonStyled from "../atoms/button-styled";
 import InputStyled from "../atoms/input-styled";
 import TextAreaStyled from "../atoms/text-area-styled";
+import DropdownStyled from "../atoms/dropdown-styled";
 
 type FormGameCardProp = {
   game: Game;
@@ -12,16 +13,22 @@ type FormGameCardProp = {
       | react.ChangeEvent<HTMLInputElement>
       | react.ChangeEvent<HTMLTextAreaElement>
   ) => void;
+  gameOptions: string[];
 };
 
-const FormGameCard = ({ game, onSubmit, handleForm }: FormGameCardProp) => {
+const FormGameCard = ({
+  game,
+  onSubmit,
+  handleForm,
+  gameOptions,
+}: FormGameCardProp) => {
   return (
     <form
       onSubmit={onSubmit}
       className="w-full flex flex-col rounded-lg space-x-2"
     >
       <div className="flex flex-col md:flex-row md:justify-between">
-        <div className="w-1/4 m-0 md:mr-10 flex flex-col items-center">
+        <div className="m-0 md:mr-10">
           <div className="text-lavanderBlush text-sm my-2 w-full">
             <label htmlFor="name">Name</label>
             <InputStyled
@@ -38,6 +45,13 @@ const FormGameCard = ({ game, onSubmit, handleForm }: FormGameCardProp) => {
               onChange={handleForm}
               placeholder="Description"
               id="description"
+            />
+          </div>
+          <div>
+            <DropdownStyled
+              options={gameOptions}
+              title="Category"
+              placeholder="select an option"
             />
           </div>
         </div>
