310b78b567d402eecb552842a502bff51c325e4e
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-14 15:59:00 +0200

    add value and classname props

--- src/components/atoms/dropdown-styled.tsx
+++ src/components/atoms/dropdown-styled.tsx
@@ -6,6 +6,8 @@ type DropdownStyledProps = {
   placeholder: string;
   onChange?: (e: React.ChangeEvent<HTMLSelectElement>) => void;
   name?: string
+  value?: string,
+  className?: string
 };
 
 const DropdownStyled = ({
@@ -14,19 +16,22 @@ const DropdownStyled = ({
   placeholder,
   onChange,
   name,
+  value,
+  className,
 }: DropdownStyledProps) => {
   return (
-    <div className="w-full">
+    <div className={`w-full text-raisingBlack ${className}`}>
       <label
         htmlFor="countries"
-        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        className="block mb-2 text-sm font-medium  text-raisingBlack"
       >
         {title}
       </label>
       <select
         name={name}
-        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="bg-gray-50 border border-gray-300 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         onChange={onChange}
+        value={value}
       >
         <option selected disabled>{placeholder}</option>
         {options.map((option) => (
