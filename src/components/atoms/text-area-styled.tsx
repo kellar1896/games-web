28463767921f316135f7c3b85e63f6a26ea08e71
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-14 14:44:46 +0200

    fix component styles

--- src/components/atoms/text-area-styled.tsx
+++ src/components/atoms/text-area-styled.tsx
@@ -14,10 +14,10 @@ const TextAreaStyled = ({
   id,
 }: TextAreaStyledProps) => {
   return (
-    <div className="relative">
+    <div className="relative w-full">
       <textarea
         id={id}
-        className="block p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="w-full block p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder={placeholder}
         value={value}
         onChange={onChange}
