01c1a86dbcd15514bc5d091eccbda3b2c7f3ac14
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-14 17:16:07 +0200

    add sorting table functions

--- src/components/molecules/games-table.tsx
+++ src/components/molecules/games-table.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useCallback } from "react";
+import React, { memo, useCallback, useMemo, useState } from "react";
 import { Game, GameHeader } from "../../models/games";
 import { camelCaseToReadable, convertIsoToDate } from "../../utils/tools";
 
@@ -15,6 +15,35 @@ const GamesTable = ({ headers, data, onEditGame }: GamesTableProps) => {
     } else return value;
   }, []);
 
+  const [sortOrder, setSortOrder] = useState("asc");
+  const [sortHeader, setSortHeader] = useState("");
+
+  const toggleSortOrder = useCallback(() => {
+    setSortOrder(sortOrder === "asc" ? "desc" : "asc");
+  }, [sortOrder]);
+
+  const handleHeaderClick = useCallback(
+    (header: GameHeader) => {
+      setSortHeader(header);
+      toggleSortOrder();
+    },
+    [toggleSortOrder]
+  );
+
+  const sortedData = useMemo(
+    () =>
+      data.sort((a: any, b: any) => {
+        if (a[sortHeader] < b[sortHeader]) {
+          return sortOrder === "asc" ? -1 : 1;
+        }
+        if (a[sortHeader] > b[sortHeader]) {
+          return sortOrder === "asc" ? 1 : -1;
+        }
+        return 0;
+      }),
+    [data, sortHeader, sortOrder]
+  );
+
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -24,15 +53,17 @@ const GamesTable = ({ headers, data, onEditGame }: GamesTableProps) => {
               <th key={index} scope="col" className="px-6 py-3">
                 <div className="flex items-center">
                   {camelCaseToReadable(header)}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="w-3 h-3 ml-1"
-                    aria-hidden="true"
-                    fill="currentColor"
-                    viewBox="0 0 320 512"
-                  >
-                    <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
-                  </svg>
+                  <button onClick={()=> handleHeaderClick(header)}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-3 h-3 ml-1"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 320 512"
+                    >
+                      <path d="M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z" />
+                    </svg>
+                  </button>
                 </div>
               </th>
             ))}
@@ -42,7 +73,7 @@ const GamesTable = ({ headers, data, onEditGame }: GamesTableProps) => {
           </tr>
         </thead>
         <tbody>
-          {data.map((row, rowIndex) => (
+          {sortedData.map((row, rowIndex) => (
             <tr
               className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
               key={rowIndex}
