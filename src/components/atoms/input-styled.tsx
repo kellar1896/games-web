a982317b89b35c371b869ad7a0b382e73d84d744
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-14 13:51:11 +0200

    fix styles

--- src/components/atoms/input-styled.tsx
+++ src/components/atoms/input-styled.tsx
@@ -22,7 +22,7 @@ const InputStyled = ({
   name,
 }: InputStyledProps) => {
   return (
-    <div className="relative">
+    <div className="w-auto">
       {icon && (
         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <FontAwesomeIcon icon={icon} className="text-gray-400" />
@@ -31,7 +31,7 @@ const InputStyled = ({
       <input
         id={id}
         type={type}
-        className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="block p-2 pl-8 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder={placeholder}
         value={value}
         onChange={onChange}
--- src/components/atoms/text-area-styled.tsx
+++ src/components/atoms/text-area-styled.tsx
@@ -17,7 +17,7 @@ const TextAreaStyled = ({
     <div className="relative">
       <textarea
         id={id}
-        className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="block p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
         placeholder={placeholder}
         value={value}
         onChange={onChange}
