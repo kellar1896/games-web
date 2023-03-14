2fb4a90190eee919115d73ce039b7fd086638217
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-03-14 12:53:56 +0200

    create help tools

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,15 +1,44 @@
 import moment from "moment";
 
 export function camelCaseToReadable(camelCaseString: string) {
-    // Replace all uppercase letters with a space followed by the lowercase version
     let readableString = camelCaseString.replace(/([A-Z])/g, ' $1');
-    // Capitalize the first letter of the string
     readableString = readableString.charAt(0).toUpperCase() + readableString.slice(1);
-    // Return the resulting string
     return readableString;
   }
 
 export function convertIsoToDate(date: string) {
-    // convert iso utc string date to redeable format
     return moment(date).format('DD/MM/YYYY');
+}
+
+export function formatString(str: string) {
+  var words = str.split('_');
+  
+  var formatted = words.map(function(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }).join(' ');
+
+  return formatted;
+}
+
+export function reverseFormatString(str: string) {
+  var words = str.split(' ');
+  
+  var reversed = words.map(function(word) {
+    return word.charAt(0).toLowerCase() + word.slice(1);
+  }).join('_');
+
+  return reversed;
+}
+
+export function createId() {
+  let result = '';
+  const length = 5;
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
 }
