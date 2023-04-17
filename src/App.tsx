8492fdc89a01364949fe6ecb5322dc4d2184c649
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-04-17 11:58:46 +0300

    call load users on app init

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Router from './router';
 import MainDashboard from './components/templates/main-dashboard';
+import { useAppDispatch } from './app/hooks';
+import { loadUsers } from './features/users/usersSlice';
 
 function App() {
+
+  const dispatch = useAppDispatch()
+
+  useEffect(()=>{
+    dispatch(loadUsers())
+  },[dispatch])
+
   return (
     <MainDashboard>
         <Router />
